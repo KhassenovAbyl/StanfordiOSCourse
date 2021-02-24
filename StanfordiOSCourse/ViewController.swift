d482cac6a623b49125584f58b32b115acf45feae
KhassenovAbyl <khassenov00@icloud.com>
2021-02-24 20:29:22 +0600

    Lecture 1

--- StanfordiOSCourse/ViewController.swift
+++ StanfordiOSCourse/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var flipCount = 0 {
+        didSet{
+            flipCountLabel.text = "Flip: \(flipCount)"
+        }
     }
-
-
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButttons: [UIButton]!
+    
+    var emojiChoises = ["🚗","🎮","🎈","🖤"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumeber = cardButttons.firstIndex(of: sender){
+            flipCard(withEmoji: emojiChoises[cardNumeber], on: sender)
+        } else {
+            print("Card not in cardButton")
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String , on button: UIButton){
+        if button.currentTitle == emoji{
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
 }
 
